85a66e9f2917e8d49e0247a47c9b92cd4082b94b
Henry <henrytsui@setsailsoftware.com>
2025-06-13 23:15:20 +0800

    week 1 q4

---
+++ week1/q4.js
@@ -0,0 +1,43 @@
+/*
+Question 4: Substring Check
+
+Problem Description:
+Write a function that checks if one string is a substring of another. Either string could be the substring of the other.
+
+Example Usage:
+isSubstring("world", "hello world");
+// Output:
+// String 1: world
+// String 2: hello world
+// Result: "world" is a substring of "hello world"
+
+isSubstring("hello world", "hello");
+// Output:
+// String 1: hello world
+// String 2: hello
+// Result: "hello" is a substring of "hello world"
+
+isSubstring("python", "javascript");
+// Output:
+// String 1: python
+// String 2: javascript
+// Result: Neither string is a substring of the other
+
+To test your solution:
+node q4.js "first string" "second string"
+*/
+
+// Get the input strings from command line arguments
+const string1 = process.argv[2];
+const string2 = process.argv[3];
+
+// Check if both inputs are provided
+if (string1 === undefined || string2 === undefined) {
+    console.log("Please provide two strings as command line arguments");
+    process.exit(1);
+}
+
+// Function to check if one string is a substring of another
+function isSubstring(str1, str2) {
+    // TODO: Implement your solution here
+}
