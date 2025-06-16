7ac04ba54cd8e9a192f7084345fb47247f75a358
chelseaSO-2000 <chelsea.sowingchi@gmail.com>
2025-06-17 00:16:21 +0800

    completed week1 q1

--- week1/q1.js
+++ week1/q1.js
@@ -33,9 +33,25 @@ const inputString = process.argv[2];
 if (inputString === undefined) {
     console.log("Please provide a string as a command line argument");
     process.exit(1);
+} else if (inputString === "") {
+    console.log("no output for empty string");
+    process.exit(2);
 }
+
+/*
+I'm confused about line 21: are you showing that an empty inputString gives no output,
+or do you want me to write code for it? When I run node q1.js "", inputString is undefined,
+and I don’t know how to log "no output for an empty string" since the first condition logs
+and exits, never execute the empty string case.
+*/
 
 // Function to print each character of a string on a new line
 function printCharacters(str) {
     // TODO: Implement your solution here
+    for (char of str) {
+        if (char === " ") continue
+        console.log(char);
+    }
 }
+
+printCharacters(inputString);
